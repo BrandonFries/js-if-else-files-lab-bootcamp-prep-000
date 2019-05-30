c3bd84c1ea760bbd3082e978982a68611c6b778d
Brandon Fries <brandonfries1993@gmail.com>
2019-05-30 18:02:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var name = "Joe"
 var height = "70"
-if(index.name === "Susan")
+if(name === "Susan")
   console.log("The name is correct")
-if(index.hegiht === "70")
+if(height === "70")
   console.log("The height is correct")
 
 // Don't worry about this
