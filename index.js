195c23ca78d45c908585722f8c72a51325018088
Brandon Fries <brandonfries1993@gmail.com>
2019-05-30 18:30:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var name = "Joe"
 var height = "70"
-
+if(name === "Joe")
+  console.log("The name is correct")
+if(height === 70)
+  console.log("The height is correct") 
+  
 // Don't worry about this
 module.exports = { name, height
 }
